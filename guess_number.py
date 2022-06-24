3146d263fc936e440019f463b5b0b1104900d915
yamayouomiya0000 <sasakiazusa0101@gmail.com>
2022-06-24 15:24:57 +0900

    Added hints guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -8,7 +8,7 @@ print('回答は',max_count,'回までです')
 
 for i in range(1,max_count+1):
     while True:
-        print(i,"回目の回答です")
+        print(i,'回目の回答です')
         num = int(input())
         if not num>=100 and num<=999:
             print('無効な入力です,3桁の数字を入力してください')
@@ -17,10 +17,15 @@ for i in range(1,max_count+1):
 
 
     if num==ans:
-        print("正解です")
+        print('正解です')
         break
-    else:
-        print("不正解です")
+    elif i==max_count:
+        print('不正解です')
+    elif num>ans:
+        print('不正解です．もっと小さい数字です')
+    elif num<ans:
+        print('不正解です．もっと大きい数字です')        
+
 else:
     print('正解は',ans,"でした")
 
