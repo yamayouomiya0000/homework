eeed62bebd40056f4f154e65bae0a51be431d0e7
yamayouomiya0000 <sasakiazusa0101@gmail.com>
2022-06-24 15:02:41 +0900

    simple number guessing guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,4 +1,19 @@
 import random
 
 ans = random.randint(100,999)
-print(ans)
+max_count = 5
+
+print('3桁の数字を当ててください')
+print('回答は',max_count,'回までです')
+
+for i in range(1,max_count+1):
+    print(i,"回目の回答です")
+    num = int(input())
+    if num==ans:
+        print("正解です")
+        break
+    else:
+        print("不正解です")
+else:
+    print('正解は',ans,"でした")
+
